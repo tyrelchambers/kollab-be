1cd7b3507ed2ac9e676d1033690349c140e4ddc8
Tyrel Chambers <tychambers3@gmail.com>
2020-09-12 08:24:20 -0400

    .

--- db/config.js
+++ db/config.js
@@ -1,14 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    host: 'localhost',
-    dialect: 'postgres',
-    database: 'kollab',
+    host: "localhost",
+    dialect: "postgres",
+    database: "kollab",
   },
   test: {
-    host: 'localhost',
-    dialect: 'postgres',
-    database: 'kollab_seed'
-  }
-}
+    host: "localhost",
+    dialect: "postgres",
+    database: "kollab_seed",
+  },
+  staging: {
+    host: process.env.STAGING_HOST,
+    dialect: "postgres",
+    database: process.env.STAGING_DATABASE,
+    username: process.env.STAGING_USERNAME,
+    password: process.env.STAGING_PASSWORD,
+    ssl: true,
+  },
+};
